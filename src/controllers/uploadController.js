75178f0b1bfa96034da099b2b585e255165f0d3e
llShivamSinghll <115917757+llShivamSinghll@users.noreply.github.com>
2025-03-11 12:03:08 +0530

    push update

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -77,7 +77,7 @@ exports.uploadResume = async (req, res) => {
             
             // Try using POST instead of GET for the matching API
             const matchResponse = await axios.post(
-                `https://blue-women-tell.loca.lt`,
+                `https://bumpy-doors-wear.loca.lt`,
                 {
                     resumeId: savedResume.id,
                     skills,
